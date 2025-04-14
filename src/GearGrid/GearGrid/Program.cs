a84b56c0acac640ec4856c89d156bdc663a8e674
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2025-04-14 14:37:34 +0200

    initial

--- src/GearGrid/GearGrid/Program.cs
+++ src/GearGrid/GearGrid/Program.cs
@@ -26,10 +26,11 @@ builder.Services.AddAuthentication(options =>
     })
     .AddIdentityCookies();
 
-var connectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+var connectionString = builder.Configuration["DefaultConnection"] ??
                        throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
-    options.UseSqlite(connectionString));
+    options.UseNpgsql(connectionString));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 builder.Services.AddIdentityCore<ApplicationUser>(options => options.SignIn.RequireConfirmedAccount = true)
