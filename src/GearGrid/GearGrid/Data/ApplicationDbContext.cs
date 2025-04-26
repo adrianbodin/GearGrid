2e61e0ceea9121e696602ccc945d14449cb411d2
Adrian Bodin Thorn <bodinthornadrian@gmail.com>
2025-04-26 21:42:46 +0200

    setup

--- src/GearGrid/GearGrid/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace GearGrid.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-    : IdentityDbContext<ApplicationUser>(options)
-{
-}
